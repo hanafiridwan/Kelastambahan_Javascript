8f232c5a5180bb8c713dfc3e683f114165f75f27
ridwanhanafi <ridwanacmad619@gmail.com>
2023-04-05 07:29:46 +0700

    mempelajari Javascript tentang variabel dan tipe data

---
+++ script.js
@@ -0,0 +1,16 @@
+// var nama = "Ridwan Hanafi"; 
+// var alamat = "Kota Tangerang";
+
+// let nama = "Ridwan Hanafi"; 
+// let alamat = "Kota Tangerang";
+
+// const nama = "Ridwan Hanafi"; 
+// const alamat = "Kota Tangerang";
+var nama = "Adit";
+var nama = "Ridwan"; //hasilnya error
+
+console.log(nama) // yg muncul ridwan. kalau let hasilnya akan error. const juga error
+
+ //ada juga yang selain var yaitu let dan const. Perbedaan dari ketiganya adalah saat menggunakan 2 var dengan variabel yang sama yg akan muncul adalah nama variabel yg kedua karena akan tertimpa. jika dengan let dengan variabel yg sama hasilnya juga eror. const juga sama. 
+
+//(nama) dan (alamat) adalah nama variabel
---
+++ script2.js
@@ -0,0 +1,7 @@
+var nama = "Udin";
+let umur = 17;
+let sudah_menikah = true;
+
+document.write(nama) //document.write berfungsi agar hasil dari nilai variabel bisa terlihat di browser
+console.log(umur) // typeof berfungsi untuk melihat tipe data.
+console.log(sudah_menikah)
