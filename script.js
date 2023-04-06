13d1d1178372f8740efba12811919978d0d4e0e7
ridwanhanafi <ridwanacmad619@gmail.com>
2023-04-06 07:53:01 +0700

    Belajar tentang kondisi1

--- script.js
+++ script.js
@@ -6,10 +6,10 @@
 
 // const nama = "Ridwan Hanafi"; 
 // const alamat = "Kota Tangerang";
-var nama = "Adit";
-var nama = "Ridwan"; //hasilnya error
+//var nama = "Adit";
+//var nama = "Ridwan"; //hasilnya error
 
-console.log(nama) // yg muncul ridwan. kalau let hasilnya akan error. const juga error
+//console.log(nama) // yg muncul ridwan. kalau let hasilnya akan error. const juga error
 
  //ada juga yang selain var yaitu let dan const. Perbedaan dari ketiganya adalah saat menggunakan 2 var dengan variabel yang sama yg akan muncul adalah nama variabel yg kedua karena akan tertimpa. jika dengan let dengan variabel yg sama hasilnya juga eror. const juga sama. 
 
